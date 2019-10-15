93c6009f645c9adc18fb9db1a9ad01e6a5d31832
jphang27 <56161279+jphang27@users.noreply.github.com>
2019-10-14 21:27:42 -0500

    finished cardio exercise 1

--- cardio1.js
+++ cardio1.js
@@ -20,7 +20,6 @@ function reverseString(str) {
 // CHALLENGE 2: VALIDATE A PALINDROME
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
-
 function isPalindrome(str) {
 
     //Compare if the str getting passed is equal to the reversed of the string
@@ -42,7 +41,6 @@ function isPalindrome(str) {
 // CHALLENGE 3: REVERSE AN INTEGER
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
-
 function reverseInt(int) {
 
     let temp = int.toString(); //Store the integer as a string
@@ -64,24 +62,76 @@ function reverseInt(int) {
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) {
-    // let temp[0] = str.toUpperCase;
-    // let temp2 = "";
-    for(let i = 0; str.length < 0; i++){
 
-        if(str.charAt(0) === toLowerCase(str) || str.substring === " "){
-            console.log(str.charAt);
-            str.charAt(0) = str.charAt(0).toUpperCase;
-            // str.substring = str.substring.toUpperCase + 1;
-            // return str;
-        }
-        else{
-            return str;
-        }
+    let strArr = str.toLowerCase().split(' '); //lowercase all the words and store it in an array.
+    console.log("outside: " + strArr); //Testing use for checking
+    //iterate through the array
+    //Add the first letter of each word from the array with the rest of the letter following each word
+    //Ex) L + ove = Love;
+    for(let i = 0; i < strArr.length; i++){
+        strArr[i] = strArr[i].substring(0, 1).toUpperCase() + strArr[i].substring(1);
     }
+    return strArr.join(" "); //put the string back together.
 
     // const upper = str.charAt(0).toUpperCase() + str.substring(1);
     // return upper;
 }
 
-let output = capitalizeLetters("i love javasccript");
-console.log(output);
+// let output = capitalizeLetters("I love javasccript");
+// console.log(output);
+
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') == 'a'
+function maxCharacter(str) {
+    const charMap = {};
+    let maxNum = 0;
+    let maxChar = '';
+  
+    str.split('').forEach(function(char) {
+      if(charMap[char]) {
+        charMap[char]++;
+      } else {
+        charMap[char] = 1;
+      }
+    });
+  
+    for(let char in charMap) {
+
+      if(charMap[char] > maxNum) {
+        maxNum = charMap[char];
+        maxChar = char;
+      }
+    }
+  
+    return maxChar;
+}
+
+// let output = "javascript";
+// console.log(maxCharacter(output));
+
+// CHALLENGE 6: FIZZBUZZ
+// Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", 
+//for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+function fizzBuzz() {
+
+    let output = "";//initialize empty string
+    for (let i = 1; i <= 100; i++){
+        //get multiples of 3's
+        if (i % 3 === 0){
+            output = output.concat("Fizz ");
+        }
+        //get multiples of 5's
+        if (i % 5 === 0){
+            output = output.concat("Buzz ");
+        }
+        //If the other numbrs are not multiple of 3's or 5's
+        if (i % 3 != 0 && i % 5 != 0){
+            output = output.concat(i.toString());
+        }
+        output = output.concat("   ");
+    }
+    console.log(output);
+
+}
+fizzBuzz();
