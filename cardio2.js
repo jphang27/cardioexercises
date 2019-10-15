0ed4307aa8d4c61e961a212678f47b7675d66f44
jphang27 <56161279+jphang27@users.noreply.github.com>
2019-10-14 22:51:09 -0500

    cardio2 finished ex) 1-4

--- cardio2.js
+++ cardio2.js
@@ -46,3 +46,62 @@ function chunkArray(arr, len) {
 //Testing
 // let arr = [1,2,3,4,5,6,7,8,9];
 // console.log(chunkArray(arr, 2));
+
+// CHALLENGE 3: FLATTEN ARRAY
+// Take an array of arrays and flatten to a single array
+// ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+function flattenArray(arrays) {
+    //The flat() method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth
+    //The global Infinity property is a numeric value representing infinity
+    return arrays.flat(Infinity);
+}
+// let arr = [[1, 2], [3, 4], [5, 6], [7]];
+// console.log(chunkArray(arr));
+
+// CHALLENGE 4: ANAGRAM
+// Return true if anagram and false if not
+// ex. 'elbow' === 'below'
+// ex. 'Dormitory' === 'dirty room##'
+function isAnagram(str1, str2) {
+    //Get 2 strings
+    //split the string into an array and then sort them and then join them together and compare the 2 strings
+    if(str1.split("").sort().join("") === str2.split("").sort().join("")){
+        //Use to check if the comparison is correct
+        // console.log(str1.split("").sort().join(""));
+        // console.log(str2.split("").sort().join(""));
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+// let output1 = "elbow";
+// let output2 = "below";
+// console.log(isAnagram(output1, output2));
+
+// CHALLENGE 5: LETTER CHANGES
+// Change every letter of the string to the one that follows it and capitalize the vowels
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'
+function letterChanges(str) {
+    const vowels = ['a', 'e', 'u', 'i', 'o']; //create an array named vowels to hold all vowels in the alphabet
+    let newStr = ""; //initialize an empty new string.
+    let letter = "";
+    for (i of str.toLowerCase()){
+        if (i.charCodeAt(0)  < 97 || i.charCodeAt(0) > 122){
+            newStr += i;
+        }
+        else {
+            letter = String.fromCharCode((i.charCodeAt(0) - 97 + 1) % 25 + 97)
+            if (vowels.includes(letter)){
+                newStr += letter.toUpperCase();
+            }
+            else {
+                newStr += letter;
+            }
+        }    
+    }
+    return newStr;
+}
+let output = "JavaScript";
+console.log(letterChanges(output));
